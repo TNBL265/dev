c5268b7f2d4829b19907c73820cf5317bc128306
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-09-03 14:59:52 +0800

    :construction: dev

--- startup_scripts/oai-cu-cp.sh
+++ startup_scripts/oai-cu-cp.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-ip addr add 10.1.20.50/24   dev eth0
-ip addr add 10.1.150.50/24  dev eth0
-ip addr add 10.1.100.50/24  dev eth0
+ip addr add 10.1.20.50/24   dev eth0 # N2
+ip addr add 10.1.150.50/24  dev eth0 # E1
+ip addr add 10.1.100.50/24  dev eth0 # F1
 
 /tini -v -- /opt/oai-gnb/bin/entrypoint.sh \ 
 /opt/oai-gnb/bin/nr-softmodem -O /opt/oai-gnb/etc/gnb.conf --sa --telnetsrv --telnetsrv.shrmod ci --log_config.global_log_options level,nocolor,time,line_num,function
--- startup_scripts/oai-du-rfsim.sh
+++ startup_scripts/oai-du-rfsim.sh
@@ -1,10 +1,5 @@
 #/bin/bash
 
-# source $(dirname $0)/utils.sh
-
-# wait_interface_up if-du-f1uc
-# ifconfig if-du-f1uc 10.1.100.10/24
-
 /tini -v -- /opt/oai-gnb/bin/entrypoint.sh \ 
 /opt/oai-gnb/bin/nr-softmodem -O /opt/oai-gnb/etc/gnb.conf --sa --telnetsrv --rfsim --log_config.global_log_options level,nocolor,time
 
