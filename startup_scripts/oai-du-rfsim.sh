ac30ff309080086e731eaf6e8a703ccf39cf7280
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-09-03 21:46:55 +0800

    :construction: s2

--- startup_scripts/oai-du-rfsim.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-/tini -v -- /opt/oai-gnb/bin/entrypoint.sh \ 
-/opt/oai-gnb/bin/nr-softmodem -O /opt/oai-gnb/etc/gnb.conf --sa --telnetsrv --rfsim --log_config.global_log_options level,nocolor,time
-
-while (true); do sleep 1; done
--- startup_scripts/oai-nr-ue-rfsim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/tini -v -- /opt/oai-nr-ue/bin/entrypoint.sh /opt/oai-nr-ue/bin/nr-uesoftmodem -O /opt/oai-nr-ue/etc/nr-ue.conf ${USE_ADDITIONAL_OPTIONS} &
-
-while true; do sleep 1; done
