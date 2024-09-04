825eea7557f046fc74bdbfed286835a6bab33b38
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-09-04 15:53:35 +0800

    :construction: clean

--- scripts/iperf3_ue.sh
+++ scripts/iperf3_ue.sh
@@ -7,9 +7,14 @@ fi
 
 ip=$1
 slice_number=$2
-
 container_name="oai-nr-ue-slice$slice_number"
-ue_ip="10.0.$slice_number.2"
 port="520$slice_number"
+ue_ip=$(docker exec -it "$container_name" hostname -I | awk '{print $1}')
 
-docker exec -it "$container_name" iperf3 -c "$ip" -B  "$ue_ip" -p "$port" -t 1000 -R
+if [ -z "$ue_ip" ]; then
+    echo "Failed to retrieve the UE IP address from container $container_name"
+    exit 1
+fi
+
+command="docker exec -it \"$container_name\" iperf3 -c \"$ip\" -B \"$ue_ip\" -p \"$port\" -t 1000 -R"
+eval $command
