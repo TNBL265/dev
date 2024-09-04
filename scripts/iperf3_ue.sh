f7f5bdfb18e2d26e8a797d16334e17823833b75e
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-09-04 15:46:23 +0800

    :construction: clean

---
+++ scripts/iperf3_ue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <IP_ADDRESS> <SLICE_NUMBER>"
+    exit 1
+fi
+
+ip=$1
+slice_number=$2
+
+container_name="oai-nr-ue-slice$slice_number"
+ue_ip="10.0.1.$slice_number"
+port = "520$slice_number"
+
+docker exec -it "$container_name" iperf3 -c "$ip" -B  "$ue_ip" -p "$port" -t 1000 -R
