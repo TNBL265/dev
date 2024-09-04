9e1a8be928753b2a31b392c923905d7ea9d43957
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-09-04 15:11:31 +0800

    :construction: clean

--- scripts/start-base.sh
+++ scripts/start-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker network create oai-net --config-from ../network-oai-net.yaml
+docker network create oai-net --config-from ../oai-net.yaml
 
 docker compose -f ../docker-compose-cn.yaml up -d
 docker compose -f ../docker-compose-ran.yaml up -d
