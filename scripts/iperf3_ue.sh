51c33ce3617df469248bcdb1ac2bd280f8c59deb
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-09-04 15:49:53 +0800

    :construction: clean

--- scripts/iperf3_ue.sh
+++ scripts/iperf3_ue.sh
@@ -9,9 +9,7 @@ ip=$1
 slice_number=$2
 
 container_name="oai-nr-ue-slice$slice_number"
-ue_ip="10.0.1.$slice_number"
+ue_ip="10.0.$slice_number.2"
 port="520$slice_number"
-
-echo docker exec -it "$container_name" iperf3 -c "$ip" -B  "$ue_ip" -p "$port" -t 1000 -R
 
 docker exec -it "$container_name" iperf3 -c "$ip" -B  "$ue_ip" -p "$port" -t 1000 -R
